e84ded8e44561730f4c82caf41d8a4be1977d891
robertaavila <roberta.avila@sc.senai.br>
2020-04-22 01:39:50 -0300

    estrutura de pilha

---
+++ linear_stack.h
@@ -0,0 +1,9 @@
+#define LENGTH 10
+
+typedef struct Stack_ Stack;
+
+Stack* create_stack();
+int push(Stack* stack, int value);
+int pop(Stack* stack, int* value);
+int top(Stack* stack, int* value);
+void free_stack(Stack* stack);
